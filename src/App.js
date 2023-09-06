61a5303397ad764a1af8085a9f424121870882c0
Nishan9t <pandeynisha9t@gmail.com>
2023-09-06 12:29:36 +0530

    map is showing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 
 import './App.css';
 import Form from './Form';
+//useJsApiLoader => it is a hook which provided weather the map script is loaded or not
+import {useJsApiLoader,GoogleMap} from '@react-google-maps/api'
 
 function App() {
+
+  const center={lat:48.8584 , lng: 2.2945}
+
+  const {isLoaded}=useJsApiLoader({
+    googleMapsApiKey:process.env.REACT_APP_GOOGLE_MAPS_API,
+  })
+
+  if(!isLoaded)
+  {
+    return <h1>map is loading</h1>
+  }
+
   return (
     <div>
      <Form/>
      <div className='absolute h-full w-full'>
      {/* google map box */}
 
+      <GoogleMap center={center} zoom={15} mapContainerClassName='w-full h-full '>
+
+      </GoogleMap>
      </div>
     </div>
   );
