6d34c9acd33c6945186a2ec369565bd9e6c42ce8
Nishan9t <pandeynisha9t@gmail.com>
2023-09-07 16:33:47 +0530

    final commit everything is done just have to do billing of api key

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-
-import { useState } from 'react';
+/*global google*/
+import { useRef, useState } from 'react';
 import './App.css';
 import Form from './Form';
 //useJsApiLoader => it is a hook which provided weather the map script is loaded or not
-import {useJsApiLoader,GoogleMap,Marker,Autocomplete} from '@react-google-maps/api'
+import {useJsApiLoader,GoogleMap,Marker,Autocomplete,DirectionsRenderer} from '@react-google-maps/api'
 
 function App() {
 
   const center={lat:48.8584 , lng: 2.2945}
 
   const [map,setMap]=useState(/**@type google.maps.Map */ (null))
+  const [directionsResponse , setDirectionsResponse]=useState(null);
+  const [distance,setDistance]=useState('')
+  const [duration,setDuration]=useState('')
+
+  //refs for input elements
+
+  /**@type React.MutableRefObject<HTMLInputElement> */
+  const originRef=useRef();
+
+/**@type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef=useRef();
 
   const {isLoaded}=useJsApiLoader({
     googleMapsApiKey:process.env.REACT_APP_GOOGLE_MAPS_API,
@@ -21,9 +32,47 @@ function App() {
     return <h1>map is loading</h1>
   }
 
+async function calculateRoute(){
+  if(originRef.current.value ==='' || destinationRef.current.value==='')
+  {
+    return
+  }
+  const directionService = new google.maps.DirectionsService()
+  const results = await directionService.route({
+    origin:originRef.current.value,
+    destination:destinationRef.current.value,
+ /* eslint-disable */
+    travelMode:google.maps.TravelMode.DRIVING
+  })
+  setDirectionsResponse(results)
+  setDistance(results.routes[0].legs[0].distance.text)
+  setDuration(results.routes[0].legs[0].duration.text)
+
+}
+
+function clearRoute(){
+  setDirectionsResponse(null)
+  setDistance('')
+  setDuration('')
+  originRef.current.value=''
+  destinationRef.current.value=''
+}
+
+
   return (
     <div>
-     <Form map={map} setMap={setMap} center={center} Autocomplete={Autocomplete}/>
+     <Form map={map}
+      setMap={setMap}
+      center={center} 
+      Autocomplete={Autocomplete}
+      originRef={originRef}
+      destinationRef={destinationRef}
+      calculateRoute={calculateRoute}
+      clearRoute={clearRoute}
+      distance={distance}
+      duration={duration}
+      
+      />
      <div className='absolute h-full w-full'>
      {/* google map box */}
 
@@ -40,6 +89,8 @@ function App() {
       >
     <Marker position={center}/>
     <Marker position={center}/>
+
+    {directionsResponse && <DirectionsRenderer directions={directionsResponse}/>}
     
       
       </GoogleMap>
--- src/Form.jsx
+++ src/Form.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 import {MdOutlineCancelPresentation} from 'react-icons/md'
 import {BiCurrentLocation} from 'react-icons/bi'
 
-export default function Form({map,setMap,center,Autocomplete}) {
+export default function Form({map,setMap,center,Autocomplete,originRef,destinationRef,calculateRoute,clearRoute,distance,duration}) {
   return (
     <div className='mx-auto w-1/2 border border-2 drop-shadow-lg mt-2 '>
         <div className='mx-auto text-center flex flex-row'>
         <Autocomplete>
-        <input type='text' className='m-2 p-1 border border-gray-300 rounded' placeholder='Origin'/>
+        <input type='text' className='m-2 p-1 border border-gray-300 rounded' placeholder='Origin' ref={originRef}/>
         </Autocomplete>
         <Autocomplete>
-        <input type='text' className='m-2 p-1 border border-gray-300 rounded' placeholder='Destination'/>
+        <input type='text' className='m-2 p-1 border border-gray-300 rounded' placeholder='Destination' ref={destinationRef}/>
         </Autocomplete>
             
-            <button className='p-2 m-2 bg-red-400 text-white rounded'>Calculate Route</button>
-            <button className='text-3xl text-center'><MdOutlineCancelPresentation/></button>
+            <button className='p-2 m-2 bg-red-400 text-white rounded' onClick={calculateRoute}>Calculate Route</button>
+            <button className='text-3xl text-center' onClick={clearRoute}><MdOutlineCancelPresentation/></button>
             
         </div>
         <div className='flex flex-row justify-between mx-4'>
-            <h1>Distance:</h1>
-            <h1 >Durations:</h1>
+            <h1>Distance:{distance}</h1>
+            <h1 >Durations:{duration}</h1>
             <button 
             className='text-2xl border border-2 p-1 rounded-2xl hover:text-blue-300'
             onClick={()=>map.panTo(center)}>
