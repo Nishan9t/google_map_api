bfb705970a56d7ae33f0b7b380935b2a45bcf634
Nishan9t <pandeynisha9t@gmail.com>
2023-09-07 15:50:58 +0530

    Autocomplete in origin and destination box is done but not working because we have buy the api -key

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
+import { useState } from 'react';
 import './App.css';
 import Form from './Form';
 //useJsApiLoader => it is a hook which provided weather the map script is loaded or not
-import {useJsApiLoader,GoogleMap,Marker} from '@react-google-maps/api'
+import {useJsApiLoader,GoogleMap,Marker,Autocomplete} from '@react-google-maps/api'
 
 function App() {
 
   const center={lat:48.8584 , lng: 2.2945}
 
+  const [map,setMap]=useState(/**@type google.maps.Map */ (null))
+
   const {isLoaded}=useJsApiLoader({
     googleMapsApiKey:process.env.REACT_APP_GOOGLE_MAPS_API,
+    libraries:['places'],
   })
 
   if(!isLoaded)
@@ -19,7 +23,7 @@ function App() {
 
   return (
     <div>
-     <Form/>
+     <Form map={map} setMap={setMap} center={center} Autocomplete={Autocomplete}/>
      <div className='absolute h-full w-full'>
      {/* google map box */}
 
@@ -30,9 +34,14 @@ function App() {
         zoomControl:false,
         streetViewControl:false,
         mapTypeControl:false,
-        fullscreenControl:false
-      }}>
+        fullscreenControl:false 
+      }}
+      onLoad={(map)=>setMap(map)}
+      >
     <Marker position={center}/>
+    <Marker position={center}/>
+    
+      
       </GoogleMap>
      </div>
     </div>
--- src/Form.jsx
+++ src/Form.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import {MdOutlineCancelPresentation} from 'react-icons/md'
 import {BiCurrentLocation} from 'react-icons/bi'
 
-export default function Form() {
+export default function Form({map,setMap,center,Autocomplete}) {
   return (
-    <div className='mx-auto w-1/2 border border-2 drop-shadow-lg mt-2'>
-        <div className='mx-auto text-center'>
-            <input type='text' className='m-2 p-1 border border-gray-300 rounded' placeholder='Destination'/>
-            <input type='text' className='m-2 p-1 border border-gray-300 rounded' placeholder='Origin'/>
+    <div className='mx-auto w-1/2 border border-2 drop-shadow-lg mt-2 '>
+        <div className='mx-auto text-center flex flex-row'>
+        <Autocomplete>
+        <input type='text' className='m-2 p-1 border border-gray-300 rounded' placeholder='Origin'/>
+        </Autocomplete>
+        <Autocomplete>
+        <input type='text' className='m-2 p-1 border border-gray-300 rounded' placeholder='Destination'/>
+        </Autocomplete>
+            
             <button className='p-2 m-2 bg-red-400 text-white rounded'>Calculate Route</button>
             <button className='text-3xl text-center'><MdOutlineCancelPresentation/></button>
             
@@ -15,7 +20,11 @@ export default function Form() {
         <div className='flex flex-row justify-between mx-4'>
             <h1>Distance:</h1>
             <h1 >Durations:</h1>
-            <button className='text-2xl'><BiCurrentLocation/></button>
+            <button 
+            className='text-2xl border border-2 p-1 rounded-2xl hover:text-blue-300'
+            onClick={()=>map.panTo(center)}>
+
+            <BiCurrentLocation/></button>
         </div>     
     </div>
   )
